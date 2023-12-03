255b0ecc8b4fbeff3840f01692f72b919bbf4adb
devwithandrei <devwithandrei@gmail.com>
2023-12-03 22:03:15 +0100

    first commit

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("91cc15e1-3a41-404d-afe0-61efcf3e7a96");
 
   return (
     <Container>
--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -1,24 +1,48 @@
-import { Billboard } from "@/types";
+"use client"
+
+import React, { useEffect, useState } from 'react';
+import { Billboard as BillboardType } from "@/types";
 
 interface BillboardProps {
-  data: Billboard;
+  data: BillboardType;
 }
 
-const Billboard: React.FC<BillboardProps> = ({
-  data
-}) => {
-  console
-  return ( 
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  const [displayText, setDisplayText] = useState('');
+  const textToType = data.label;
+  const maxLength = textToType.length;
+
+  useEffect(() => {
+    let currentIndex = 0;
+    let timeout: NodeJS.Timeout;
+
+    const animateText = () => {
+      setDisplayText(textToType.substring(0, currentIndex));
+      currentIndex = (currentIndex + 1) % (maxLength + 1);
+      timeout = setTimeout(animateText, 200); // Adjust the speed here (milliseconds)
+    };
+
+    animateText();
+
+    return () => clearTimeout(timeout);
+  }, [textToType, maxLength]);
+
+  return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-            {data.label}
+            <div className="Typewriter" data-testid="typewriter-wrapper">
+              <span className="Typewriter__wrapper">
+                {displayText}
+              </span>
+              <span className="Typewriter__cursor">|</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-   );
+  );
 };
 
 export default Billboard;

397e98c6cb80808f6755033881ccca7bd847045d
devwithandrei <devwithandrei@gmail.com>
2023-12-14 00:18:21 +0100

    final

---
+++ app/(routes)/about/page.tsx
@@ -0,0 +1,22 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const AboutPage = async () => {
+  const billboard = await getBillboard("bc02fec3-88b2-4379-bd0a-0643d42ccea0");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+      </div>
+    </Container>
+  )
+};
+
+export default AboutPage;
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,44 @@
+import React from 'react';
+
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
-      <div className="mx-auto py-10">
-        <p className="text-center text-xs text-black">
-          &copy; 2023 Trendy Stocky, Inc. All rights reserved.
+      <div className="mx-auto py-3">
+        <p className="text-center text-sm md:text-lg font-bold text-deminBlue">
+          <a href="/" className="hover:underline hover:no-underline hover:text-blue-500 transition-colors duration-300">
+            Trendy Stocky
+          </a>
+        </p>
+      </div>
+      <div className="mx-auto py-4 flex flex-wrap justify-center">
+        <p className="text-center text-sm md:text-lg text-black mr-8">
+          <a href="/about" className="hover:underline hover:no-underline hover:text-blue-500 transition-colors duration-300">
+            About Us
+          </a>
+        </p>
+        <p className="text-center text-sm md:text-lg text-black mr-8">
+          <a href="/privacy-policy" className="hover:underline hover:no-underline hover:text-blue-500 transition-colors duration-300">
+            Privacy & Policy
+          </a>
+        </p>
+        <p className="text-center text-sm md:text-lg text-black mr-8">
+          <a href="/return-policies" className="hover:underline hover:no-underline hover:text-blue-500 transition-colors duration-300">
+            Return Policies
+          </a>
+        </p>
+        <p className="text-center text-sm md:text-lg text-black">
+          <a href="/contact-us" className="hover:underline hover:no-underline hover:text-blue-500 transition-colors duration-300">
+            Contact Us
+          </a>
+        </p>
+      </div>
+      <div className="mx-auto py-3">
+        <p className="text-center text-sm md:text-lg text-black">
+          &copy; {new Date().getFullYear()} Trendy Stocky, Inc. All rights reserved.
         </p>
       </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;

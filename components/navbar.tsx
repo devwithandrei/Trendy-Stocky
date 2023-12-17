ab3ab9bca3dc9b9d5c8ed644be5b06b723b78843
devwithandrei <devwithandrei@gmail.com>
2023-12-17 02:14:08 +0100

    some changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,11 +13,12 @@ const Navbar = async () => {
         <div className="relative px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16">
           <Link href="/" className="ml-4 flex gap-x-2">
             <p className="font-bold text-sm md:text-xl"> {/* Set different font size for mobile and larger screens */}
-              <span style={{ color: '#C12800' }}>Trendy</span>{" "}
-              <span style={{ color: '#0056AB' }}>Stocky</span>
+              <span style={{ color: '#C12800' }}>T</span>{" "}
+              <span style={{ color: '#0056AB' }}>S</span>
             </p>
           </Link>
-          <div className="ml-auto" style={{ marginLeft: '-10px' }}>
+          <div className="flex-grow"></div> {/* Empty div to push NavbarActions to the right */}
+          <div className="mr-4"> {/* Give some right margin to NavbarActions */}
             <NavbarActions />
           </div>
           <MainNav data={categories} />

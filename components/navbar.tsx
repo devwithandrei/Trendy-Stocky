34613659352405973f52151a17cffe0c6abb2f5c
devwithandrei <devwithandrei@gmail.com>
2023-12-17 02:29:52 +0100

    anotherone

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,25 +7,24 @@ import getCategories from "@/actions/get-categories";
 const Navbar = async () => {
   const categories = await getCategories();
 
-  return ( 
+  return (
     <div className="border-b sticky top-0 z-50 bg-white">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16">
           <Link href="/" className="ml-4 flex gap-x-2">
-            <p className="font-bold text-sm md:text-xl"> {/* Set different font size for mobile and larger screens */}
-              <span style={{ color: '#C12800' }}>Trendy</span>{" "}
-              <span style={{ color: '#0056AB' }}>Stocky</span>
+            <p className="font-bold text-sm md:text-xl">
+              <span style={{ color: '#C12800' }}>T</span>{" "}
+              <span style={{ color: '#0056AB' }}>S</span>
             </p>
           </Link>
-          <div className="flex-grow"></div> {/* Empty div to push NavbarActions to the right */}
-          <div className="mr-4"> {/* Give some right margin to NavbarActions */}
+          <MainNav data={categories} />
+          <div className="ml-auto sm:ml-0 sm:mr-4"> {/* Keep the original margin on larger screens */}
             <NavbarActions />
           </div>
-          <MainNav data={categories} />
         </div>
       </Container>
     </div>
   );
 };
- 
+
 export default Navbar;

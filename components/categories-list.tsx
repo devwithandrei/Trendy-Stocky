12f7bc3e24343d0a4652c9d78b50b97ac40592ee
devwithandrei <devwithandrei@gmail.com>
2023-12-31 01:07:53 +0100

    updating

--- components/categories-list.tsx
+++ components/categories-list.tsx
@@ -24,43 +24,43 @@ const CategoriesList: React.FC = () => {
         // Other category properties
       },
       {
-        id: '32d4c5b3-839f-4c57-bb98-694a99ea2e4e',
+        id: '8503f9ee-50fa-4fb2-9440-1b0f1779d1b2',
         name: 'Jeans',
         imageUrl: 'https://media.giphy.com/media/Z9uId9712lo9gfJvil/giphy.gif',
         // Other category properties
       },
       {
-        id: '58636a43-e64c-44a3-ab73-4b66cdd6e173',
+        id: 'f1fc66bb-c0da-4de5-b788-5ae970c1378e',
         name: 'T-Shirts',
         imageUrl: 'https://media.giphy.com/media/zbGUB1RQEOmyEVjJvK/giphy.gif',
         // Other category properties
       },
       {
-        id: 'd3163d3b-854a-4d26-a2de-b323762943e1',
+        id: 'a128723f-590b-4a78-b08b-77fb3605ae8d',
         name: 'Shoes',
         imageUrl: 'https://media.giphy.com/media/14gnhUPQZpdyLe/source.gif',
         // Other category properties
       },
       {
-        id: 'e5c096e4-00dd-4d60-b9a8-39a560612b71',
+        id: '411385ba-cf30-4cb5-a91a-e91214eb2147',
         name: 'Accesories',
         imageUrl: 'https://media.giphy.com/media/vy0dVKp8BLOUxvvrj6/giphy.gif',
         // Other category properties
       },
       {
-        id: '21fe2c3a-f9fe-48a9-bccb-dd1c8f20be97',
+        id: '5a4c8e6e-cae4-4326-8d2c-1714f98eb7a4',
         name: 'For Him',
         imageUrl: 'https://th.bing.com/th/id/R.0d92d6bbbb6049a60abf58e53b1935e6?rik=%2fCwM2W0jniJAAw&riu=http%3a%2f%2f66.media.tumblr.com%2ff1a14725f46003edd4959a5d359775f5%2ftumblr_o7ezwnDuGE1tcsj92o2_r1_400.gif&ehk=JqMwmWLg2z7szLglCF8vIyjm1EeN4TzappilLWb74cI%3d&risl=&pid=ImgRaw&r=0',
         // Other category properties
       },
       {
-        id: '39550a39-89ad-4f8a-9e8d-36088d57a383',
+        id: 'd052eba5-e7b5-4a84-862c-6938c31ffb9f',
         name: 'For Her',
         imageUrl: 'https://media.giphy.com/media/yyc8oPpz22B6h0g6bm/giphy.gif',
         // Other category properties
       },
       {
-        id: '0e1fda06-ea32-47b3-b4d2-0c0ac9d89e7a',
+        id: '58b2b963-16eb-4514-b364-2763a5f9d7a3',
         name: 'For Kids',
         imageUrl: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-03/14/10/enhanced/webdr06/anigif_enhanced-19388-1457965470-2.gif',
         // Other category properties

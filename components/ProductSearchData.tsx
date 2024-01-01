2bb33a1169c5d7b92d54a6db17c5f7a31485762b
devwithandrei <devwithandrei@gmail.com>
2024-01-01 16:46:51 +0100

    updating

--- components/ProductSearchData.tsx
+++ components/ProductSearchData.tsx
@@ -2,136 +2,6 @@
 
 export const products = [
     {
-      "id": "9f401a28-2383-442f-83b6-f65c9686d4dc",
-      "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-      "categoryId": "9e2c7068-d057-4728-8d5e-012d22f63e22",
-      "name": "dark marina blue",
-      "price": "€123.00",
-      "isFeatured": true,
-      "isArchived": false,
-      "sizeId": "69a7e9d6-5207-458f-b4c8-1c7352f2cd91",
-      "descriptionId": "d3e138ca-ca22-41c1-a319-f688580eee39",
-      "brandId": "f19b6e59-401f-4ad3-9055-f1115bc5a8b7",
-      "colorId": "85d7a4ea-5bd1-4b75-853a-cd513b13bb7b",
-      "createdAt": "2023-12-28T04:54:47.679Z",
-      "updatedAt": "2023-12-28T04:54:47.679Z",
-      "images": [
-        {
-          "id": "bf55ff11-abae-401d-9b74-03a3f9fb6f22",
-          "productId": "9f401a28-2383-442f-83b6-f65c9686d4dc",
-          "url": "https://res.cloudinary.com/dvaxtwnfa/image/upload/v1703739263/r6libvomrukgga6aohuc.webp",
-          "createdAt": "2023-12-28T04:54:47.679Z",
-          "updatedAt": "2023-12-28T04:54:47.679Z"
-        }
-        // Add more images if necessary
-      ],
-      "category": {
-        "id": "9e2c7068-d057-4728-8d5e-012d22f63e22",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "billboardId": "bdc3ae6e-993e-4280-b0ae-5ba868c0e6ed",
-        "name": "Jackets",
-        "createdAt": "2023-12-28T03:52:48.860Z",
-        "updatedAt": "2023-12-28T03:52:48.860Z"
-      },
-      "color": {
-        "id": "85d7a4ea-5bd1-4b75-853a-cd513b13bb7b",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "black",
-        "value": "#black",
-        "createdAt": "2023-12-28T04:05:49.658Z",
-        "updatedAt": "2023-12-28T04:05:49.658Z"
-      },
-      "size": {
-        "id": "69a7e9d6-5207-458f-b4c8-1c7352f2cd91",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "XS",
-        "value": "XS",
-        "createdAt": "2023-12-28T04:04:34.653Z",
-        "updatedAt": "2023-12-28T04:04:34.653Z"
-      },
-      "brand": {
-        "id": "f19b6e59-401f-4ad3-9055-f1115bc5a8b7",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "Nike",
-        "value": "Nike",
-        "createdAt": "2023-12-28T04:04:48.325Z",
-        "updatedAt": "2023-12-28T13:13:14.363Z"
-      },
-      "description": {
-        "id": "d3e138ca-ca22-41c1-a319-f688580eee39",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "Nice product description",
-        "value": "Nice product description. Nice product description. Nice product description. Nice product description.",
-        "createdAt": "2023-12-28T04:05:32.588Z",
-        "updatedAt": "2023-12-28T04:05:32.588Z"
-      }
-    },
-    {
-      "id": "f7281e35-aee9-446f-9b3f-e276d81fac21",
-      "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-      "categoryId": "9e2c7068-d057-4728-8d5e-012d22f63e22",
-      "name": "Nike Jordan",
-      "price": "€132.99",
-      "isFeatured": true,
-      "isArchived": false,
-      "sizeId": "69a7e9d6-5207-458f-b4c8-1c7352f2cd91",
-      "descriptionId": "d3e138ca-ca22-41c1-a319-f688580eee39",
-      "brandId": "f19b6e59-401f-4ad3-9055-f1115bc5a8b7",
-      "colorId": "85d7a4ea-5bd1-4b75-853a-cd513b13bb7b",
-      "createdAt": "2023-12-28T04:06:39.249Z",
-      "updatedAt": "2023-12-28T04:07:38.963Z",
-      "images": [
-        {
-          "id": "0563ddac-833a-420b-a862-120b353f9bb7",
-          "productId": "f7281e35-aee9-446f-9b3f-e276d81fac21",
-          "url": "https://res.cloudinary.com/dvaxtwnfa/image/upload/v1703736372/mgcp0cnt6nidyfjhhqxk.jpg",
-          "createdAt": "2023-12-28T04:07:39.001Z",
-          "updatedAt": "2023-12-28T04:07:39.001Z"
-        },
-        // Add more images if necessary
-      ],
-      "category": {
-        "id": "9e2c7068-d057-4728-8d5e-012d22f63e22",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "billboardId": "bdc3ae6e-993e-4280-b0ae-5ba868c0e6ed",
-        "name": "Jackets",
-        "createdAt": "2023-12-28T03:52:48.860Z",
-        "updatedAt": "2023-12-28T03:52:48.860Z"
-      },
-      "color": {
-        "id": "85d7a4ea-5bd1-4b75-853a-cd513b13bb7b",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "black",
-        "value": "#black",
-        "createdAt": "2023-12-28T04:05:49.658Z",
-        "updatedAt": "2023-12-28T04:05:49.658Z"
-      },
-      "size": {
-        "id": "69a7e9d6-5207-458f-b4c8-1c7352f2cd91",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "XS",
-        "value": "XS",
-        "createdAt": "2023-12-28T04:04:34.653Z",
-        "updatedAt": "2023-12-28T04:04:34.653Z"
-      },
-      "brand": {
-        "id": "f19b6e59-401f-4ad3-9055-f1115bc5a8b7",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "Nike",
-        "value": "Nike",
-        "createdAt": "2023-12-28T04:04:48.325Z",
-        "updatedAt": "2023-12-28T13:13:14.363Z"
-      },
-      "description": {
-        "id": "d3e138ca-ca22-41c1-a319-f688580eee39",
-        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-        "name": "Nice product description",
-        "value": "Nice product description. Nice product description. Nice product description. Nice product description.",
-        "createdAt": "2023-12-28T04:05:32.588Z",
-        "updatedAt": "2023-12-28T04:05:32.588Z"
-      }
-    },
-    {
       "id": "7148e65c-e8c8-48b8-9819-c84413df7fda",
         "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
         "categoryId": "8503f9ee-50fa-4fb2-9440-1b0f1779d1b2",
@@ -196,8 +66,8 @@ export const products = [
       "brand": {
           "id": "7c76c3e3-6b8c-4e73-a9e0-5e258714254c",
           "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
-          "name": "armani",
-          "value": "armani",
+          "name": "Diesel",
+          "value": "Diesel",
           "createdAt": "2023-12-29T00:59:55.202Z",
           "updatedAt": "2023-12-29T00:59:55.202Z"
       },
@@ -210,5 +80,69 @@ export const products = [
           "updatedAt": "2023-12-28T04:05:32.588Z"
       }
   },
+  {
+    "id": "f7281e35-aee9-446f-9b3f-e276d81fac21",
+        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
+        "categoryId": "a128723f-590b-4a78-b08b-77fb3605ae8d",
+        "name": "Jordan Model 1",
+        "price": "132.99",
+        "isFeatured": true,
+        "isArchived": false,
+        "sizeId": "69a7e9d6-5207-458f-b4c8-1c7352f2cd91",
+        "descriptionId": "d3e138ca-ca22-41c1-a319-f688580eee39",
+        "brandId": "f19b6e59-401f-4ad3-9055-f1115bc5a8b7",
+        "colorId": "85d7a4ea-5bd1-4b75-853a-cd513b13bb7b",
+        "createdAt": "2023-12-28T04:06:39.249Z",
+        "updatedAt": "2024-01-01T01:32:55.765Z",
+        "images": [
+            {
+                "id": "8a2eeef5-c733-45e0-b69e-9afd82eb81c1",
+                "productId": "f7281e35-aee9-446f-9b3f-e276d81fac21",
+                "url": "https://res.cloudinary.com/dvaxtwnfa/image/upload/v1704072754/nlpmfx5m8krbdmforqqk.webp",
+                "createdAt": "2024-01-01T01:32:55.810Z",
+                "updatedAt": "2024-01-01T01:32:55.810Z"
+            }
+      // Add more images if necessary
+    ],
+    "category": {
+        "id": "a128723f-590b-4a78-b08b-77fb3605ae8d",
+        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
+        "billboardId": "d3163d3b-854a-4d26-a2de-b323762943e1",
+        "name": "Shoes",
+        "createdAt": "2023-12-31T00:04:16.817Z",
+        "updatedAt": "2023-12-31T00:04:16.817Z"
+    },
+    "color": {
+        "id": "85d7a4ea-5bd1-4b75-853a-cd513b13bb7b",
+        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
+        "name": "black",
+        "value": "#black",
+        "createdAt": "2023-12-28T04:05:49.658Z",
+        "updatedAt": "2023-12-28T04:05:49.658Z"
+    },
+    "size": {
+        "id": "69a7e9d6-5207-458f-b4c8-1c7352f2cd91",
+        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
+        "name": "xs",
+        "value": "xs",
+        "createdAt": "2023-12-28T04:04:34.653Z",
+        "updatedAt": "2023-12-28T04:04:34.653Z"
+    },
+    "brand": {
+        "id": "f19b6e59-401f-4ad3-9055-f1115bc5a8b7",
+        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
+        "name": "nike",
+        "value": "nike",
+        "createdAt": "2023-12-28T04:04:48.325Z",
+        "updatedAt": "2023-12-28T13:13:14.363Z"
+    },
+    "description": {
+        "id": "d3e138ca-ca22-41c1-a319-f688580eee39",
+        "storeId": "3363b6c8-c22d-4a1a-be1e-e4f788fe8f45",
+        "name": "nice product description",
+        "value": "nice product description nice product description   nice product description nice product description",
+        "createdAt": "2023-12-28T04:05:32.588Z",
+        "updatedAt": "2023-12-28T04:05:32.588Z"
+    }
+},
   ];
-

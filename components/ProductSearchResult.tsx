9dd1d61ad72eb0359f1737ed30d376bca1682c57
devwithandrei <devwithandrei@gmail.com>
2024-01-01 19:32:56 +0100

    updated

--- components/ProductSearchResult.tsx
+++ components/ProductSearchResult.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { Product, Category } from '@/SearchTypes';
+import { Product } from '@/SearchTypes';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -10,34 +10,20 @@ interface ProductSearchResultProps {
 }
 
 const ProductSearchResult: React.FC<ProductSearchResultProps> = ({ product }) => {
-  const [category, setCategory] = useState<Category | null>(null);
-
-  useEffect(() => {
-    // Simulate fetching category data based on the categoryId
-    // Replace this with your actual API call or data retrieval logic to fetch the category
-    // For example:
-    // fetchCategory(product.categoryId)
-    //   .then((data) => setCategory(data))
-    //   .catch((error) => console.error('Error fetching category:', error));
-    // This is a placeholder with empty array dependencies, so it runs once on mount
-  }, []);
 
   const productUrl = `/product/${product.id}`; // Replace 'product.id' with the actual ID property
 
   return (
     <Link href={productUrl}>
       <div className="block p-2 border-b hover:bg-gray-100 cursor-pointer bg-opacity-75" style={{ backgroundColor: 'rgba(0, 0, 255, 0.2)' }}>
-        <div className="flex items-center">
-          <div className="w-20 h-20 rounded-md overflow-hidden">
+        <h3 className="font-semibold text-black mb-2">{product.name}</h3> {/* Adjusted margin to position title */}
+        <div className="flex items-start"> {/* Changed to 'items-start' to align content at the start */}
+          <div className="w-20 h-20 rounded-md overflow-hidden" style={{ marginTop: '0.5rem', marginRight: '1rem' }}> {/* Adjusted marginTop and marginRight */}
             <Image src={product.images[0]?.url} alt={product.name} layout="responsive" width={80} height={80} />
           </div>
           <div className="ml-4">
-            <h3 className="font-semibold text-black">{product.name}</h3>
             <p className="text-sm text-black">{product.description.value}</p>
             <p className="text-sm font-semibold text-black">{product.price}</p>
-            {category && (
-              <p className="text-sm text-gray-500">Category: {category.name}</p>
-            )}
           </div>
         </div>
       </div>
